aae7d24395ad55c6f78959c49dc52884658bbdbf
David Stirling <daves1990@hotmail.co.uk>
2022-10-03 11:19:07 +0100

    Add NGFF reader

--- cellprofiler_core/constants/reader/__init__.py
+++ cellprofiler_core/constants/reader/__init__.py
@@ -1,7 +1,12 @@
+import re
+
 builtin_readers = {
     "imageio_reader": "ImageIOReader",
+    "ngff_reader": "NGFFReader",
     "bioformats_reader": "BioformatsReader",
 }
 all_readers = dict()
 bad_readers = []
+
+ZARR_FILETYPE = re.compile(r"(?<=\.zarr)", flags=re.IGNORECASE)
 
