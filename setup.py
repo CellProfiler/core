6422faf28492fe9b0e9a006a1900cd426d0fc0c3
David Stirling <daves1990@hotmail.co.uk>
2022-10-11 13:44:12 +0100

    Test new build system

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8",
     url="https://github.com/CellProfiler/core",
-    version="4.2.4",
+    version="5.0.0",
     zip_safe=False,
 )
