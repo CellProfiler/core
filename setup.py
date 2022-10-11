d0f2da048183632ef7d35e6c90947fb3b4e9ceb6
David Stirling <daves1990@hotmail.co.uk>
2022-10-11 17:19:36 +0100

    Require zarr

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setuptools.setup(
         "pyzmq~=22.3",
         "scikit-image>=0.16.2",
         "scipy>=1.4.1",
+        "zarr>=2.10",
     ],
     license="BSD",
     name="cellprofiler-core",
