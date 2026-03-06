aacddce9287594f3afef53a921e0c9e0ef54c3b2
Nodar Gogoberidze <gnodar01@gmail.com>
2026-03-06 13:31:51 -0500

    [linux] Bump to 4.2.8.1

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests"]),
     python_requires=">=3.8",
     url="https://github.com/CellProfiler/core",
-    version="4.2.8",
+    version="4.2.8.1",
     zip_safe=False,
 )
